1d2e57c8b85222864213556e7a1af42fa16da2e3
Fawzi Gharib <fawz11gharib@gmail.com>
2024-03-23 22:44:13 +0300

    REFACTOR: Project Structure updated

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1 className="w-full bg-red-500 text-center">I'm NavBar</h1>
+    </div>
+  );
+};
+
+export default NavBar;
