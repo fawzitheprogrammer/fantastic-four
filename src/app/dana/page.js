c6c6bd4a06e90edafe2095259d35ab6a44e240f0
Fawzi Gharib <fawz11gharib@gmail.com>
2024-04-10 23:17:30 +0300

    Footer commit

---
+++ src/app/dana/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dana() {
+  return (
+    <div className='p-4 mt-[500px]'>
+      <h1 className='text-black font-semibold p-4 bg-red-600 '>HELKLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLO</h1>
+    </div>
+  )
+}
